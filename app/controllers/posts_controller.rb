6568f31648984dfe02e7b3327de31289f8eda91f
Andrew-Tinsley <Andrew.Tinsley@kellogg.northwestern.edu>
2026-03-08 20:51:00 -0500

    Associate posts with current user and guard new/create against unauthenticated access

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,17 +1,23 @@
 class PostsController < ApplicationController
-
   def index
     @posts = Post.all
   end
 
   def new
+    if @current_user == nil
+      redirect_to "/login"
+    end
   end
-  
+
   def create
+    if @current_user == nil
+      redirect_to "/login"
+      return
+    end
     @post = Post.new
     @post["body"] = params["body"]
     @post["image"] = params["image"]
-    # TODO: assign logged-in user as user that created the post
+    @post.user = @current_user
     @post.save
     redirect_to "/posts"
   end

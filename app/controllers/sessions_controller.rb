9bfa568382f741f8910d14e4cb333857fd98af64
Andrew-Tinsley <Andrew.Tinsley@kellogg.northwestern.edu>
2026-03-08 20:50:35 -0500

    Implement login authentication and logout in SessionsController

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,20 @@
 class SessionsController < ApplicationController
   def new
   end
-  
+
   def create
-    # TODO: authenticate user
-    flash["notice"] = "Nope."
+    @user = User.find_by(email: params["email"])
+    if @user != nil && @user.authenticate(params["password"])
+      session[:user_id] = @user.id
+      redirect_to "/posts"
+    else
+      flash["notice"] = "Invalid email or password."
+      redirect_to "/login"
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
     redirect_to "/login"
   end
 end
